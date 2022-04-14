06a902cc26f9b72b97698a67e46fd44b0b991b60
hamzasehouli <sehoulihamza@yahoo.com>
2022-04-14 10:13:16 +0000

    Created splash screen

---
+++ screens/splash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const SplashScreen = function () {
+  return (
+    <View>
+      <Text>splash screen</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
