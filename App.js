028072ded95415401227f17bcf879bed571af7c3
hamzasehouli <sehoulihamza@yahoo.com>
2022-04-14 10:37:39 +0000

    Installed and added react native navigation

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-const App = function () {
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = function () {
   return (
     <View>
-      <Text>Shopp app</Text>
+      <Text> homepage</Text>
     </View>
   );
+};
+const CartScreen = function () {
+  return (
+    <View>
+      <Text> cart</Text>
+    </View>
+  );
+};
+
+const StackNavigator = (
+  <Stack.Navigator initialRouteName="Home">
+    <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+    <Stack.Screen name="Cart" component={CartScreen}></Stack.Screen>
+  </Stack.Navigator>
+);
+
+const App = function () {
+  return <NavigationContainer>{StackNavigator}</NavigationContainer>;
 };
 
 export default App;
