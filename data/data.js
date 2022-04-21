57b414ea8791ff7658737b6d4274369c406925eb
hamzasehouli <sehoulihamza@yahoo.com>
2022-04-21 13:35:33 +0000

    Aded more sneaker data, restyled ApparelScreen

--- data/data.js
+++ data/data.js
@@ -227,13 +227,29 @@ export const SNEAKERS = [
   new Apparel(
     's1',
     'Hiking trainers',
-    39.99,
     'https://static.pullandbear.net/2/photos//2022/V/1/2/p/2313/940/002/01/2313940002_2_1_8.jpg?t=1642439839013&imwidth=750',
+    39.99,
+    'Sneaker',
   ),
   new Apparel(
     's2',
     'Retro casual trainers',
-    19.99,
     'https://static.pullandbear.net/2/photos//2022/V/1/2/p/2215/940/100/01/2215940100_2_1_8.jpg?t=1642679701509&imwidth=750',
+    19.99,
+    'Sneaker',
+  ),
+  new Apparel(
+    's3',
+    'Hiking trainers',
+    'https://static.pullandbear.net/2/photos//2022/V/1/2/p/2302/940/002/2302940002_2_1_8.jpg?t=1637073135601&imwidth=750',
+    39.99,
+    'Sneaker',
+  ),
+  new Apparel(
+    's4',
+    'Knit trainers',
+    'https://static.pullandbear.net/2/photos//2022/V/1/2/p/2300/940/030/2300940030_2_1_8.jpg?t=1640598803685&imwidth=750',
+    22.99,
+    'Sneaker',
   ),
 ];
--- screens/ApparelScreen.js
+++ screens/ApparelScreen.js
@@ -1,17 +1,90 @@
-import React, {useLayoutEffect} from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
+import {SNEAKERS} from '../data/data';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Colors from '../constants/Colors';
+import BaseText from '../components/BaseText';
 
 const Apparel = function (props) {
-  useLayoutEffect(() => {
+  useEffect(() => {
     props.navigation.setOptions({
       title: props.route.params.title,
     });
-  });
+  }, []);
+  const renderItemHandler = function (item) {
+    return (
+      <TouchableOpacity style={styles.box}>
+        <ImageBackground
+          source={{uri: item.item.imageUrl}}
+          resizeMode="cover"
+          style={styles.imageBackground}>
+          <TouchableOpacity style={styles.favorite}>
+            <Icon
+              size={25}
+              color={Colors.primaryColor}
+              name="ios-heart-outline"></Icon>
+          </TouchableOpacity>
+        </ImageBackground>
+        <View style={styles.details}>
+          <BaseText>{item.item.title}</BaseText>
+          <BaseText color={Colors.primaryColor} style={{fontWeight: '700'}}>
+            ${item.item.price}
+          </BaseText>
+        </View>
+      </TouchableOpacity>
+    );
+  };
   return (
-    <View>
-      <Text>Apparel</Text>
+    <View style={styles.screen}>
+      <FlatList
+        numColumns={2}
+        data={SNEAKERS}
+        renderItem={renderItemHandler}></FlatList>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: 'white',
+    flex: 1,
+    height: '100%',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  box: {
+    width: '50%',
+    height: 300,
+    position: 'relative',
+    marginBottom: 40,
+    marginRight: 5,
+  },
+  imageBackground: {
+    position: 'relative',
+    flex: 1,
+  },
+  favorite: {
+    position: 'absolute',
+    right: 10,
+    top: 10,
+    backgroundColor: 'white',
+    borderRadius: 10000000,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 40,
+    height: 40,
+  },
+  details: {
+    padding: 5,
+    height: '20%',
+  },
+});
 
 export default Apparel;
