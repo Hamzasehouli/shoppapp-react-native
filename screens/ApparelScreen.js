b01fa219e22177c291b10a294bc1b9603d190ad3
hamzasehouli <sehoulihamza@yahoo.com>
2022-04-22 11:48:16 +0000

    Added JEANSWOMEN

--- screens/ApparelScreen.js
+++ screens/ApparelScreen.js
@@ -16,6 +16,7 @@ import {JACKETS} from '../data/JACKETS.js';
 import {SUITS} from '../data/SUITS.js';
 import {SHOES} from '../data/SHOES.js';
 import {SHOESWOMEN} from '../data/women/SHOESWOMEN.js';
+import {JEANSWOMEN} from '../data/women/JEANSWOMEN.js';
 import {BAGSWOMEN} from '../data/women/BAGSWOMEN.js';
 import {SPORTWEAR} from '../data/SPORTWEAR.js';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -61,6 +62,9 @@ const Apparel = function (props) {
         break;
       case 'Bags women':
         setData(BAGSWOMEN);
+        break;
+      case 'Jeans women':
+        setData(JEANSWOMEN);
         break;
 
       default:
