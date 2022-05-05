375632b536520ccff35e8756504101d248a736cd
hamzasehouli <sehoulihamza@yahoo.com>
2022-05-05 13:52:29 +0000

    Fixed the vulnerability in the package.json and Gemfile

--- android/app/src/main/java/com/bnkinp/MainApplication.java
+++ android/app/src/main/java/com/bnkinp/MainApplication.java
@@ -14,7 +14,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.facebook.react.bridge.JSIModulePackage; 
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
-import io.invertase.firebase.app.ReactNativeFirebaseAppPackage
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -32,7 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          packages.add(new ReactNativeFirebaseAppPackage());
+          // packages.add(new ReactNativeFirebaseAppPackage());
           return packages;
         }
 
