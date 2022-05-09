63430cad4c713903c16338bc8dba79d04ef5e90d
hamzasehouli <sehoulihamza@yahoo.com>
2022-05-09 16:19:12 +0100

    Added more items images

--- data/men/CAPS.js
+++ data/men/CAPS.js
@@ -6,7 +6,11 @@ export const CAPS = [
     'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/504/800/4829504800_2_1_8.jpg?t=1636998265370&imwidth=750',
     9.99,
     'Caps',
-    [],
+    [
+      'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/504/800/4829504800_2_5_8.jpg?t=1636998265370&imwidth=750',
+      'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/504/800/4829504800_2_3_8.jpg?t=1636998265370&imwidth=750',
+      'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/504/800/4829504800_2_4_8.jpg?t=1636998265370&imwidth=750',
+    ],
   ),
   new Apparel(
     'ca2',
@@ -14,7 +18,10 @@ export const CAPS = [
     'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/505/802/4829505802_2_1_8.jpg?t=1636998273154&imwidth=750',
     9.99,
     'Caps',
-    [],
+    [
+      'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/505/802/4829505802_2_4_8.jpg?t=1636998273154&imwidth=750',
+      'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/505/802/4829505802_2_3_8.jpg?t=1636998273154&imwidth=750',
+    ],
   ),
   new Apparel(
     'ca3',
@@ -22,7 +29,11 @@ export const CAPS = [
     'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/511/015/4829511015_2_1_8.jpg?t=1637935332199&imwidth=750',
     9.99,
     'Caps',
-    [],
+    [
+      'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/511/015/4829511015_2_5_8.jpg?t=1637935332199&imwidth=750',
+      'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/511/015/4829511015_2_4_8.jpg?t=1637935332199&imwidth=750',
+      'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/511/015/4829511015_2_3_8.jpg?t=1637935332199&imwidth=750',
+    ],
   ),
   new Apparel(
     'ca4',
@@ -30,6 +41,10 @@ export const CAPS = [
     'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/521/800/4829521800_2_1_8.jpg?t=1647872444970&imwidth=750',
     9.99,
     'Caps',
-    [],
+    [
+      'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/521/800/4829521800_2_5_8.jpg?t=1647872444970&imwidth=750',
+      'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/521/800/4829521800_2_3_8.jpg?t=1647872444970&imwidth=750',
+      'https://static.pullandbear.net/2/photos//2022/V/0/2/p/4829/521/800/4829521800_2_4_8.jpg?t=1647872444970&imwidth=750',
+    ],
   ),
 ];
--- data/women/BAGSWOMEN.js
+++ data/women/BAGSWOMEN.js
@@ -2,11 +2,16 @@ import Apparel from '../../models/Apparel';
 export const BAGSWOMEN = [
   new Apparel(
     'bw1',
-    'Blue Solid Shoulder Bag',
-    'https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2034456/2017/9/16/11505543457239-DressBerry-Blue-Solid-Shoulder-Bag-9621505543457381-1.jpg',
+    'Sea Green Solid Structured Handheld Bag',
+    'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15861522/2021/12/1/5243ef96-2f07-4bca-9f09-96f3c90bc4b11638342176595LinoPerrosSeaGreenSolidStructuredHandheldBag1.jpg',
     17.99,
     'Bag',
-    [],
+    [
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15861522/2021/12/1/f04e68ee-50f2-49f3-a201-3dd0b78fc8871638342177119LinoPerrosSeaGreenSolidStructuredHandheldBag5.jpg',
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15861522/2021/12/1/c774a7f4-ba81-44f4-8e93-712beda28e311638342177023LinoPerrosSeaGreenSolidStructuredHandheldBag4.jpg',
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15861522/2021/12/1/e5c1cb64-0dda-4d7b-be83-eac67aefac2c1638342176953LinoPerrosSeaGreenSolidStructuredHandheldBag3.jpg',
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15861522/2021/12/1/8d3e1667-4d3a-4cef-8b95-8eaebd17bf691638342176884LinoPerrosSeaGreenSolidStructuredHandheldBag2.jpg',
+    ],
   ),
   new Apparel(
     'bw2',
@@ -14,7 +19,11 @@ export const BAGSWOMEN = [
     'https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/16570714/2022/4/18/3c349ff2-a630-4824-be47-8c523321541a1650283679173HMWomenWhiteSmallShoulderBag1.jpg',
     37.99,
     'Bag',
-    [],
+    [
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/16570714/2022/4/18/9cd44167-7204-42a3-a573-72822fb59a5d1650283679193HMWomenWhiteSmallShoulderBag15.jpg',
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/16570714/2022/4/18/7ea94e1f-0b96-4802-a786-0147280946a01650283679215HMWomenWhiteSmallShoulderBag3.jpg',
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/16570714/2022/4/18/eb88a938-c6b4-4132-8c81-9775d8d22a971650283679208HMWomenWhiteSmallShoulderBag2.jpg',
+    ],
   ),
   new Apparel(
     'bw2',
@@ -22,16 +31,25 @@ export const BAGSWOMEN = [
     'https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/17327910/2022/3/30/adca5734-a8f3-4951-8418-d3f5253796521648642525204-Tommy-Hilfiger-Pink-Textured-PU-Structured-Sling-Bag-9121648-1.jpg',
     37.99,
     'Bag',
-    [],
+    [
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/17327910/2022/3/30/8abfa2d7-ff1a-4750-a342-1eb9720f0eb11648642525141-Tommy-Hilfiger-Pink-Textured-PU-Structured-Sling-Bag-9121648-7.jpg',
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/17327910/2022/3/30/3828f15f-4240-4597-a3ec-06b42a1fb9091648642525171-Tommy-Hilfiger-Pink-Textured-PU-Structured-Sling-Bag-9121648-4.jpg',
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/17327910/2022/3/30/a7fc73c2-be4b-4080-bf6d-2faf925582ac1648642525193-Tommy-Hilfiger-Pink-Textured-PU-Structured-Sling-Bag-9121648-2.jpg',
+    ],
     true,
     11.99,
   ),
   new Apparel(
     'bw2',
-    'Green Solid Handheld Bag',
-    'https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/productimage/2021/3/11/aaa9fe7e-281c-422f-9184-e4e145e3557f1615482997561-1.jpg',
+    'Mauve Pink Solid PU Regular Structured Handheld Bag',
+    'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/17331714/2022/3/15/5ec7b2bc-d5a7-4850-90e1-993d0211d0001647322117907-Allen-Solly-Mauve-Pink-Solid-PU-Regular-Structured-Handheld--1.jpg',
     37.99,
     'Bag',
-    [],
+    [
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/17331714/2022/3/15/fadf5e2f-e3a9-4e8a-bec2-9bca0a9957ff1647322117866-Allen-Solly-Mauve-Pink-Solid-PU-Regular-Structured-Handheld--6.jpg',
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/17331714/2022/3/25/e0d8ee96-89a2-45dd-85ed-db6d6b9011961648180976522-Allen-Solly-Mauve-Pink-Solid-PU-Regular-Structured-Handheld--11.jpg',
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/17331714/2022/3/15/5230b3c3-d29a-4c99-b07a-9b9226a2dd1b1647322117874-Allen-Solly-Mauve-Pink-Solid-PU-Regular-Structured-Handheld--5.jpg',
+      'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/17331714/2022/3/15/215c4999-c5b3-4200-abe1-39646a6370791647322117899-Allen-Solly-Mauve-Pink-Solid-PU-Regular-Structured-Handheld--2.jpg',
+    ],
   ),
 ];
