539ed73c9e2dca1e834f270d69da936bd097982e
hamzasehouli <sehoulihamza@yahoo.com>
2022-06-06 16:54:21 +0100

    Added webview lib

--- screens/PaymentScreen.js
+++ screens/PaymentScreen.js
@@ -1,31 +1,9 @@
 // import {View, useStripe} from '@stripe/stripe-react-native';
+import {WebView} from 'react-native-webview';
 import React from 'react';
 import {View, Text} from 'react-native';
 function PaymentScreen() {
-  return (
-    <View>
-      <View
-        postalCodeEnabled={true}
-        placeholders={{
-          number: '4242 4242 4242 4242',
-        }}
-        cardStyle={{
-          backgroundColor: '#FFFFFF',
-          textColor: '#000000',
-        }}
-        style={{
-          width: '100%',
-          height: 50,
-          marginVertical: 30,
-        }}
-        onCardChange={cardDetails => {
-          console.log('cardDetails', cardDetails);
-        }}
-        onFocus={focusedField => {
-          console.log('focusField', focusedField);
-        }}></View>
-    </View>
-  );
+  return <WebView source={{uri: 'https://avito.ma'}} />;
 }
 
 export default PaymentScreen;
